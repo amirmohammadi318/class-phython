5417140b424abc06da9aca140cb167a10081b8f1
amirmohammadi318 <88482846+amirmohammadi318@users.noreply.github.com>
2021-08-26 11:45:22 +0430

    Create S4.P04.py

---
+++ S4.P04.py
@@ -0,0 +1,6 @@
+# class-phythonlist_old = ["amin" , "Reza", "hosein", "sina", "hadi", "ghasem"]
+list_new = ['sina', 'hadi', 'ghasem']
+list_final = list_ghadimi + list_gadid
+print(list_nahaei)
+print(len(set(list_nahaei)), 'asami Names')
+print(set(list_nahaei))
